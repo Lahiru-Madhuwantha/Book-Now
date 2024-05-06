98dabba75ca3c17cb21eeafa60cbbd6a2c518949
Lahiru Madhuwantha <lahirumadhuwantha293@gmail.com>
2024-05-06 10:14:08 +0530

    Add Sass support and update layout and NavBar components

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
+import NavBar from './components/NavBar/NavBar'
+import './layout.scss'
+
 function App() {
   return (
-    <div>Hello World</div>
+    <div className="layout">
+      <NavBar/>
+    </div>
   )
 }
 
