13fe710a3817cd53e5c70aa6cd2e669f50500930
Lahiru Madhuwantha <lahirumadhuwantha293@gmail.com>
2024-05-06 12:11:03 +0530

    Add SearchBar component and update HomePage layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import NavBar from './components/NavBar/NavBar'
 import './layout.scss'
+import HomePage from './routes/HomePage'
 
 function App() {
   return (
     <div className="layout">
+      <div className="navbar">
       <NavBar/>
+      </div>
+      <div className="content">
+        <HomePage/>
+      </div>
     </div>
   )
 }
