64a88916663b14cf410db33954f0cd865b744780
Lahiru Madhuwantha <lahirumadhuwantha293@gmail.com>
2024-05-18 09:07:48 +0530

    Add new components and data for Card, Filter, and ListPage

--- src/routes/listPage/ListPage.tsx
+++ src/routes/listPage/ListPage.tsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import './listPage.scss'
+import { listData } from '../../lib/dummyData'
+import Filter from '../../components/Filter/Filter';
 
 function ListPage() {
-  return (
-    <div>ListPage</div>
-  )
+
+  const data = listData;
+  return <div className="listPage">
+  <div className="listContainer">
+    <div className="wrapper">
+      <Filter/>
+      {/* {data.map(item=>(
+        <Card key={item.id} item={item}/>
+      ))} */}
+    </div>
+  </div>
+  <div className="mapContainer">
+    {/* <Map items={data}/> */}
+  </div>
+</div>;
 }
 
 export default ListPage
