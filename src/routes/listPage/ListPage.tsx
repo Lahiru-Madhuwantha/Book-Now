aafb784dbcb5b247565a1e56da4cc14322c9a1af
Lahiru Madhuwantha <lahirumadhuwantha293@gmail.com>
2024-05-18 12:34:37 +0530

    Styles Filter

--- src/routes/listPage/ListPage.tsx
+++ src/routes/listPage/ListPage.tsx
@@ -2,23 +2,27 @@ import React from 'react'
 import './listPage.scss'
 import { listData } from '../../lib/dummyData'
 import Filter from '../../components/Filter/Filter';
+import Card from '../../components/Card/Card';
+
 
 function ListPage() {
 
   const data = listData;
-  return <div className="listPage">
-  <div className="listContainer">
-    <div className="wrapper">
-      <Filter/>
-      {/* {data.map(item=>(
-        <Card key={item.id} item={item}/>
-      ))} */}
+  return (
+    <div className="listPage">
+      <div className="listContainer">
+        <div className="wrapper">
+          <Filter/>
+          {data.map(item => (
+            <Card key={item.id} {...item} />
+          ))}
+        </div>
+      </div>
+      <div className="mapContainer">
+        {/* <Map items={data}/> */}
+      </div>
     </div>
-  </div>
-  <div className="mapContainer">
-    {/* <Map items={data}/> */}
-  </div>
-</div>;
+  );
 }
 
 export default ListPage
